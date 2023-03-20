5c3dd3b43def829a74cd5d5c65aea8e770c96563
willyhuang <willy810308@gmail.com>
2023-03-21 00:27:00 +0800

    U34 done.

--- alpha_camp/movie_list/app.js
+++ alpha_camp/movie_list/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
-const { engine } = require('express-handlebars')
+const engine = require('express-handlebars').engine
 const port = 3000
 
 // express template engine
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
