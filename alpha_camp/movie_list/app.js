f89170cb708c6d77296dc615b0ccfc9240197b1d
willyhuang <willy810308@gmail.com>
2023-03-21 23:27:23 +0800

    U42 done.

--- alpha_camp/movie_list/app.js
+++ alpha_camp/movie_list/app.js
@@ -17,17 +17,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies/:movie_id', (req, res) => {
-    console.log('movie_id', req.params)
-    const movieOne = {
-        title: 'Jurassic World: Fallen Kingdom',
-        image: 'c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg',
-        release_date: '2018-06-06',
-        description: `
-        Several years after the demise of Jurassic World, a volcanic eruption threatens the remaining dinosaurs on the
-        island of Isla Nublar. Claire Dearing, the former park manager and founder of the Dinosaur Protection Group,
-        recruits Owen Grady to help prevent the extinction of the dinosaurs once again.`
-    }
-    res.render('show', { movie: movieOne })
+    //console.log('movie_id', req.params)
+    const movie = movieList.results.filter(item => item.id == req.params.movie_id)
+    res.render('show', { movie: movie[0] })
+})
+
+app.get('/search', (req, res) => {
+    const movies = movieList.results.filter(item => {
+        return item.title.toLowerCase().includes(req.query.keyword.toLowerCase())
+    })
+
+    res.render('index', { movies: movies, keyword: req.query.keyword })
 })
 
 // strat and listen on the Express server
