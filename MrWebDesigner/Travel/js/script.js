c106fe518d139539f9ece08d30776448ab3d0741
willyhuang <willyhuang@flytech.com.tw>
2023-05-10 13:34:50 +0800

    Travel done.

--- MrWebDesigner/Travel/js/script.js
+++ MrWebDesigner/Travel/js/script.js
@@ -5,3 +5,33 @@ menu.onclick = () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+var swiper = new Swiper(".home-slider", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let loadMoreBtn = document.querySelector(".packages .load-more .btn");
+let currentItem = 6;
+
+loadMoreBtn.onclick = () => {
+  let boxes = [...document.querySelectorAll(".packages .box-container .box")];
+
+  for (var i = currentItem; i < currentItem + 3; i++) {
+
+    boxes[i].style.display = "inline-block";
+  };
+  currentItem += 3;
+  console.log(currentItem);
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = "none";
+  }
+}
