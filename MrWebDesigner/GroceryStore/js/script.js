0a6beaefacb9b90aa0d9b8ae81842611be1afd74
willyhuang <willyhuang@flytech.com.tw>
2023-06-02 18:02:11 +0800

    add GroceryStore

---
+++ MrWebDesigner/GroceryStore/js/script.js
@@ -0,0 +1,39 @@
+let searchForm = document.querySelector('.search-form');
+let shoppingCart = document.querySelector('.shopping-cart');
+let loginForm = document.querySelector('.login-form');
+let navbar = document.querySelector('.navbar');
+
+document.getElementById('search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+document.getElementById('cart-btn').onclick = () => {
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+document.getElementById('login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+document.getElementById('menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+}
